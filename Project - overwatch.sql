09ccc3e374a3995e4c81b807069be7dfab6839b3
zaighy <m.zaighumali@gmail.com>
2017-04-25 19:27:07 +0500

    Overwatch Database SQL
    overwatch db in mysql

---
+++ Project - overwatch.sql
@@ -0,0 +1,99 @@
+#drop database if exists overwatch;
+create database overwatch;
+
+use overwatch;
+
+CREATE TABLE heroes (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    description VARCHAR(5000),
+    role VARCHAR(90),
+    health INT,
+    armour INT,
+    shield INT,
+    real_name VARCHAR(90),
+    age INT,
+    affiliaton VARCHAR(500),
+    base_of_operations VARCHAR(500),
+    difficulty INT(3)
+);
+
+CREATE TABLE abilities (
+    id INT,
+    name VARCHAR(100),
+    description VARCHAR(5000),
+    is_ultimate VARCHAR(10),
+    CONSTRAINT ability_frn FOREIGN KEY (id)
+        REFERENCES heroes (id)
+);
+
+CREATE TABLE game_mode (
+    id INT primary key,
+    name VARCHAR(90)
+);
+
+CREATE TABLE brawl (
+    id INT PRIMARY KEY,
+    start_date VARCHAR(90)
+);
+
+CREATE TABLE brawl_type (
+    id INT,
+    name VARCHAR(100),
+    CONSTRAINT brawl_frn FOREIGN KEY (id)
+        REFERENCES brawl (id)
+);
+
+CREATE TABLE maps (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    location VARCHAR(100),
+    mode VARCHAR(50)
+);
+
+CREATE TABLE users (
+    battletag VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE user_stats (
+    battletag VARCHAR(100),
+    time_played VARCHAR(50),
+    SR INT,
+    losses INT,
+    wins INT,
+    winrate VARCHAR(50),
+    total_kills INT,
+    total_deaths INT,
+    level INT,
+    CONSTRAINT userstats_frn FOREIGN KEY (battletag)
+        REFERENCES users (battletag)
+);
+
+CREATE TABLE user_hero_stats (
+    battletag VARCHAR(100),
+    hero_id INT,
+    wins INT,
+    losses INT,
+    winrate VARCHAR(50),
+    accuracy VARCHAR(50),
+    time_played VARCHAR(50),
+    CONSTRAINT userherostats_users_frn FOREIGN KEY (battletag)
+        REFERENCES users (battletag),
+    CONSTRAINT userherostats_heroes_frn FOREIGN KEY (hero_id)
+        REFERENCES heroes (id)
+);
+
+CREATE TABLE game (
+    id int primary key,
+    maps INT,
+    hero INT,
+    brawl INT,
+    users varchar(100),
+    game_mode INT,
+    CONSTRAINT game_mp_frn FOREIGN KEY (maps) REFERENCES maps(id),
+    CONSTRAINT game_hr_frn FOREIGN KEY (hero) REFERENCES heroes(id),
+    CONSTRAINT game_brl_frn FOREIGN KEY (brawl) REFERENCES brawl(id),
+    CONSTRAINT game_usr_frn FOREIGN KEY (users) REFERENCES users(battletag),
+    CONSTRAINT game_md_frn FOREIGN KEY (game_mode) REFERENCES game_mode(id)
+);
+
